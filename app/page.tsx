481313097a91f66672c04aca58a017ce9d0df078
Susmita-Dey <susmitadey475@gmail.com>
2025-05-02 19:22:57 +0530

    nextjs init and add simple coming soon page

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+// Coming Soon in Home Page
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-orange-200 text-gray-950">
+      <h1 className="text-4xl font-bold mb-4">Coming Soon</h1>
+      <p className="text-lg text-gray-600">
+        Our website is under construction. Stay tuned!
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
